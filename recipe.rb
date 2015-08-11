12c59603a6ef3ff82e8c70243f37fb1fbf86a079
furoshiki32 <kawada.hirosi@gmail.com>
2015-08-11 14:23:05 +0900

    レシピに説明を追加。レシピ検索でID省略を可能に

--- recipe.rb
+++ recipe.rb
@@ -1,16 +1,22 @@
 require './recipe_data'
 
 class Recipe
-  def initialize(id)
-    recipe = RecipeData.find_recipe_data(id)
-    print_data(recipe)
+  def initialize(id=nil)
+    if id
+      recipe = RecipeData.find_recipe_data(id)
+      print_data(recipe)
+    else
+      RecipeData.each_all do |recipe|
+        print_data(recipe)
+      end
+    end
   end
 
   private
 
   def print_data(recipe)
-    puts "#{recipe[:id]}: #{recipe[:name]}"
+    puts "#{recipe[:id]}: #{recipe[:name]} : #{recipe[:discription]}"
   end
 end
 
-Recipe.new(2)
+Recipe.new()
--- recipe_data.rb
+++ recipe_data.rb
@@ -2,21 +2,29 @@ class RecipeData
   DATA = [
     {
       id:   1,
-      name: 'オムライス'
+      name: 'オムライス',
+      discription: '卵を焼いてごはんにのせる'
     },
     {
       id:   2,
-      name: '親子丼'
+      name: '親子丼',
+      discription: '鶏肉を焼いて卵でとじてごはんにのせる'
     },
     {
       id:   3,
-      name: '杏仁豆腐'
+      name: '杏仁豆腐',
+      discription: '牛乳と砂糖をまぜてゼラチンで固める'
     }
   ].freeze
 
   class << self
     def find_recipe_data(id)
       DATA.find {|recipe| recipe[:id] == id }
+    end
+    def each_all
+      DATA.each do |recipe|
+        yield recipe
+      end
     end
   end
 end

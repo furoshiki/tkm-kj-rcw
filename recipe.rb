13fbe1f926ed14546109aafd506a7f77e33aa34d
furoshiki32 <kawada.hirosi@gmail.com>
2015-08-11 14:45:07 +0900

    ユーザ名でレシピを検索できるように

--- recipe.rb
+++ recipe.rb
@@ -4,14 +4,9 @@ require './recipe_data'
 class RecipeReader
   class << self
     # IDを元にレシピを検索し、標準出力する
-    def print(id=nil)
-      if id
-        recipe = RecipeData.find_recipe_data(id)
+    def print(id=nil,user_name=nil)
+      RecipeData.find_recipe(id,user_name) do |recipe|
         print_recipe(recipe)
-      else
-        RecipeData.each_all do |recipe|
-          print_recipe(recipe)
-        end
       end
     end
 
@@ -24,8 +19,8 @@ class RecipeReader
 end
 
 # コマンドライン引数を元にレシピ情報への操作を決定する
-if ARGV[0]
-  RecipeReader.print(ARGV[0].to_i)
+if ARGV[1]
+  RecipeReader.print(ARGV[0],ARGV[1].to_i)
 else
-  RecipeReader.print()
+  RecipeReader.print(ARGV[0])
 end
--- recipe_data.rb
+++ recipe_data.rb
@@ -3,26 +3,33 @@ class RecipeData
     {
       id:   1,
       name: 'オムライス',
+      user_name: 'hoge',
       discription: '卵を焼いてごはんにのせる'
     },
     {
       id:   2,
       name: '親子丼',
+      user_name: 'kou',
       discription: '鶏肉を焼いて卵でとじてごはんにのせる'
     },
     {
       id:   3,
       name: '杏仁豆腐',
+      user_name: 'piyo',
       discription: '牛乳と砂糖をまぜてゼラチンで固める'
     }
   ].freeze
 
   class << self
-    def find_recipe_data(id)
-      DATA.find {|recipe| recipe[:id] == id }
-    end
-    def each_all
-      DATA.each do |recipe|
+    def find_recipe( user_name=nil, id=nil )
+      list = DATA;
+      if id
+        list = list.select {|recipe| recipe[:id] == id }
+      end
+      if user_name
+        list = list.select {|recipe| recipe[:user_name] == user_name }
+      end
+      list.each do |recipe|
         yield recipe
       end
     end

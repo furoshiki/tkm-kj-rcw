bd8bb37fd4fb83abe9cdb6c52c2d4ca9dda2657b
Takumi Kaji <ivezuki3@gmail.com>
2015-08-11 12:22:30 +0900

    spec5: 個々のレシピにIDを振り出した

--- recipe_data.rb
+++ recipe_data.rb
@@ -1,7 +1,16 @@
 class RecipeData
   DATA = [
-    'オムライス',
-    '親子丼',
-    '杏仁豆腐'
-  ].map(&:freeze).freeze
+    {
+      id:   1,
+      name: 'オムライス'
+    },
+    {
+      id:   2,
+      name: '親子丼'
+    },
+    {
+      id:   3,
+      name: '杏仁豆腐'
+    }
+  ].freeze
 end

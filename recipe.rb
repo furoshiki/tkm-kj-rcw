bebd97ee940bb0a42e5b781736c4f2c850033e19
furoshiki32 <kawada.hirosi@gmail.com>
2015-08-11 14:34:16 +0900

    Refactoring

--- recipe.rb
+++ recipe.rb
@@ -1,27 +1,31 @@
 require './recipe_data'
 
 # レシピの管理、出力を行えるようにするクラス
-class Recipe
-  def initialize(id=nil)
-    if id
-      recipe = RecipeData.find_recipe_data(id)
-      print_data(recipe)
-    else
-      RecipeData.each_all do |recipe|
-        print_data(recipe)
+class RecipeReader
+  class << self
+    # IDを元にレシピを検索し、標準出力する
+    def print(id=nil)
+      if id
+        recipe = RecipeData.find_recipe_data(id)
+        print_recipe(recipe)
+      else
+        RecipeData.each_all do |recipe|
+          print_recipe(recipe)
+        end
       end
     end
-  end
 
-  private
-  def print_data(recipe)
-    puts "#{recipe[:id]}: #{recipe[:name]} : #{recipe[:discription]}"
+    private
+    # レシピデータを標準出力する
+    def print_recipe(recipe)
+      puts "#{recipe[:id]}: #{recipe[:name]} : #{recipe[:discription]}"
+    end
   end
 end
 
 # 引数を元にレシピへの操作を決定する
 if ARGV[0]
-  Recipe.new(ARGV[0].to_i)
+  RecipeReader.print(ARGV[0].to_i)
 else
-  Recipe.new()
+  RecipeReader.print()
 end
